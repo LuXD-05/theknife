56d8c4a9aa6dba4a815fe22e49a386c737d21c33
VertixTVV <luca.nardo@outlook.it>
2025-04-09 20:41:27 +0200

    Added the function OnGuestButtonClick() where the guest can serch a restaurant, if the restaurant doesn't exist, an alert is displayed.

--- src/main/java/uni/insubria/theknife/controller/AppController.java
+++ src/main/java/uni/insubria/theknife/controller/AppController.java
@@ -4,12 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import uni.insubria.theknife.Main;
+import uni.insubria.theknife.model.Restaurant;
+import uni.insubria.theknife.service.RestaurantRepository;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppController {
+
+    @FXML
+    private TextField cityTextField;
 
     @FXML
     protected void onLoginButtonClick() throws IOException {
@@ -22,5 +32,24 @@ public class AppController {
     protected void onRegisterButtonClick() {}
 
     @FXML
-    protected void onGuestButtonClick() {}
+    protected void onGuestButtonClick() throws IOException {
+
+        String selectedLocation = cityTextField.getText().trim();
+
+        boolean locationExists = RestaurantRepository.loadRestaurants().stream()
+            .map(r -> r.getLocation().split(",")[0].trim())
+            .distinct()
+            .anyMatch(loc -> loc.equalsIgnoreCase(selectedLocation));
+        
+        if (locationExists) {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/home.fxml"));
+            SessionController.setScene(fxmlLoader);
+        } else {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Nessun ristorante trovato");
+            alert.setHeaderText(null);
+            alert.setContentText("Non sono presenti ristoranti nella location indicata");
+            alert.showAndWait();
+        }        
+    }
 }
