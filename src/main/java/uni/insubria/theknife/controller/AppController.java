c3809cea6ff8ea86c5b509e2515999c4faae5ef3
VertixTVV <luca.nardo@outlook.it>
2025-04-10 15:18:25 +0200

    Implemented Login button function

--- src/main/java/uni/insubria/theknife/controller/AppController.java
+++ src/main/java/uni/insubria/theknife/controller/AppController.java
@@ -10,26 +10,58 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import uni.insubria.theknife.Main;
 import uni.insubria.theknife.model.Restaurant;
+import uni.insubria.theknife.model.User;
 import uni.insubria.theknife.service.RestaurantRepository;
+import uni.insubria.theknife.service.UserRepository;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class AppController {
 
     @FXML
-    private TextField cityTextField;
+    private TextField cityTextField, usernameTextField, passwordTextField;
 
     @FXML
     protected void onLoginButtonClick() throws IOException {
         //TODO validate user and password
+
+        UserRepository repo = new UserRepository();
+        Map<String, User> users = repo.loadUsers();
+
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+        User user = users.get(username);
+
+        if (user == null) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("ATTENZIONE");
+            alert.setHeaderText(null);
+            alert.setContentText("Utente non trovato");
+            alert.showAndWait();
+            return;
+        }
+
+        if (!AuthController.verify(password, user.getPassword())) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("ATTENZIONE");
+            alert.setHeaderText(null);
+            alert.setContentText("Password errata");
+            alert.showAndWait();
+            return;
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/home.fxml"));
         SessionController.setScene(fxmlLoader);
     }
 
     @FXML
-    protected void onRegisterButtonClick() {}
+    protected void onRegisterButtonClick() {
+        String hashedPassword = AuthController.hash(passwordTextField.getText());
+    }
 
     @FXML
     protected void onGuestButtonClick() throws IOException {
@@ -46,9 +78,9 @@ public class AppController {
             SessionController.setScene(fxmlLoader);
         } else {
             Alert alert = new Alert(AlertType.WARNING);
-            alert.setTitle("Nessun ristorante trovato");
+            alert.setTitle("ATTENZIONE");
             alert.setHeaderText(null);
-            alert.setContentText("Non sono presenti ristoranti nella location indicata");
+            alert.setContentText("Nessun ristorante trovato nella location indicata");
             alert.showAndWait();
         }        
     }
